313923fbe4f577edd53220ff17bef2f71d9b8085
Mike Caron <caron.mike@outlook.com>
2025-07-28 22:55:01 -0400

    Automation (#1)
    * start the automation process
    
    * add cache to the build step
    
    * test what artifacts look like
    
    * add both the release and pages jobs
    
    * fix permission
    
    * fix permission again
    
    * allow manually triggering builds

--- makeBundle.sh
+++ makeBundle.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+mkdir dist
+mkdir dist/data
+mkdir dist/data/sprites
 
 node data.mjs
-zip tappdex.zip config.json icon.png index.html data/pokemon.json data/sprites/*
+cp data/pokemon.json dist/data/
+cp index.html dist/
+cp icon.png dist/
+cp config.json dist/
+cp data/sprites/* dist/data/sprites/
+
+#cd dist
+#zip ../tappdex.zip config.json icon.png index.html data/pokemon.json data/sprites/*
+#cd ..
